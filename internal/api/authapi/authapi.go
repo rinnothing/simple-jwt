f29f3f5a7840588381a2be861ceb330a04186c9b
rinnothing <58170199+rinnothing@users.noreply.github.com>
2025-07-22 13:34:29 +0300

    add logs to api

--- internal/api/authapi/authapi.go
+++ internal/api/authapi/authapi.go
@@ -1,10 +1,13 @@
 package authservice
 
 import (
+	"errors"
 	"net/http"
+	"slices"
 
 	"github.com/labstack/echo/v4"
 	"github.com/rinnothing/simple-jwt/internal/api/schema"
+	"github.com/rinnothing/simple-jwt/internal/repository/postgres"
 	"github.com/rinnothing/simple-jwt/internal/service/auth"
 	storage "github.com/rinnothing/simple-jwt/internal/service/secure_storage"
 
@@ -35,15 +38,18 @@ func NewAPI(auth auth.AuthService, storage storage.StorageService, logger *zap.L
 
 func (a *APIImpl) AuthorizeGUID(e echo.Context, guid string) error {
 	ctx := e.Request().Context()
+	a.logRequest(e, "authorize", zap.String("guid", guid))
 
 	uuid, err := a.storage.PutGUID(ctx, guid)
 	if err != nil {
+		a.logger.Error("can't put guid in storage", zap.Error(err))
 		return InternalError(e)
 	}
 
 	// TODO: don't forget to set e.IPExtractor to echo.ExtractIPDirect()
 	pair, err := a.auth.IssueTokens(ctx, string(uuid), e.Request().UserAgent(), e.RealIP())
 	if err != nil {
+		a.logger.Error("can't issue tokens", zap.Error(err))
 		return InternalError(e)
 	}
 
@@ -52,6 +58,7 @@ func (a *APIImpl) AuthorizeGUID(e echo.Context, guid string) error {
 
 func (a *APIImpl) GetGUID(e echo.Context, params schema.GetGUIDParams) error {
 	ctx := e.Request().Context()
+	a.logRequest(e, "get_guid", zap.String("access_token", params.AccessToken))
 
 	err := a.tryAuthorize(e, params.AccessToken)
 	if err != nil {
@@ -60,11 +67,13 @@ func (a *APIImpl) GetGUID(e echo.Context, params schema.GetGUIDParams) error {
 
 	uuid, err := a.auth.GetUUID(ctx, params.AccessToken)
 	if err != nil {
+		a.logger.Error("can't get uuid from access token", zap.Error(err))
 		return InternalError(e)
 	}
 
 	guid, err := a.storage.GetGUID(ctx, uuid)
 	if err != nil {
+		a.logger.Error("can't get guid from storage", zap.Error(err))
 		return InternalError(e)
 	}
 
@@ -75,17 +84,29 @@ func (a *APIImpl) RefreshTokens(e echo.Context) error {
 	ctx := e.Request().Context()
 
 	var pair schema.TokenPair
-	e.Bind(&pair)
+	err := e.Bind(&pair)
+	if err != nil {
+		a.logger.Error("can't unmarshal request", zap.Error(err))
+		return BadRequest(e, err.Error())
+	}
 
-	err := a.tryAuthorize(e, *pair.AccessToken)
+	a.logRequest(e, "refresh", zap.String("access_token", *pair.AccessToken), zap.String("refresh_token", *pair.RefreshToken))
+
+	err = a.tryAuthorize(e, *pair.AccessToken)
 	if err != nil {
 		return err
 	}
 
 	// TODO: don't forget to set e.IPExtractor to echo.ExtractIPDirect()
 	newPair, err := a.auth.RefreshTokens(ctx, pair, e.Request().UserAgent(), e.RealIP())
-	if err != nil {
+	if errors.Is(err, postgres.ErrWrongUserAgent) {
+		a.logger.Info("refresh token denied", zap.String("access_token", string(*pair.AccessToken)),
+			zap.String("refresh_token", string(*pair.RefreshToken)))
 		return Unauthorized(e)
+	}
+	if err != nil {
+		a.logger.Error("can't refresh tokens", zap.Error(err))
+		return InternalError(e)
 	}
 
 	return e.JSON(http.StatusOK, newPair)
@@ -94,6 +115,8 @@ func (a *APIImpl) RefreshTokens(e echo.Context) error {
 func (a *APIImpl) Unauthorize(e echo.Context, params schema.UnauthorizeParams) error {
 	ctx := e.Request().Context()
 
+	a.logRequest(e, "unauthorize", zap.String("access_token", params.AccessToken))
+
 	err := a.tryAuthorize(e, params.AccessToken)
 	if err != nil {
 		return err
@@ -101,6 +124,7 @@ func (a *APIImpl) Unauthorize(e echo.Context, params schema.UnauthorizeParams) e
 
 	err = a.auth.Unauthorize(ctx, params.AccessToken)
 	if err != nil {
+		a.logger.Error("can't unauthorize user", zap.Error(err))
 		return InternalError(e)
 	}
 
@@ -110,10 +134,21 @@ func (a *APIImpl) Unauthorize(e echo.Context, params schema.UnauthorizeParams) e
 func (a *APIImpl) tryAuthorize(e echo.Context, token schema.AccessToken) error {
 	allow, err := a.auth.HasAccess(e.Request().Context(), token)
 	if err != nil {
+		a.logger.Error("can't check access", zap.Error(err))
 		return InternalError(e)
 	}
 	if !allow {
+		a.logger.Info("access denied", zap.String("access_token", string(token)))
 		return e.NoContent(http.StatusUnauthorized)
 	}
 	return nil
+}
+
+func (a *APIImpl) logRequest(e echo.Context, name string, fields ...zap.Field) {
+	a.logger.Info("got request",
+		slices.Concat(
+			[]zap.Field{zap.String("name", name), zap.String("user_agent", e.Request().UserAgent()), zap.String("ip", e.RealIP())},
+			fields,
+		)...,
+	)
 }
--- internal/api/authapi/common.go
+++ internal/api/authapi/common.go
@@ -1,15 +1,20 @@
 package authservice
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
 func InternalError(e echo.Context) error {
-	return e.NoContent(http.StatusInternalServerError)
+	return e.String(http.StatusInternalServerError, "internal error")
 }
 
 func Unauthorized(e echo.Context) error {
-	return e.NoContent(http.StatusUnauthorized)
+	return e.String(http.StatusUnauthorized, "unauthorized")
+}
+
+func BadRequest(e echo.Context, reason string) error {
+	return e.String(http.StatusBadRequest, fmt.Sprintf("bad request, reason: %s", reason))
 }
